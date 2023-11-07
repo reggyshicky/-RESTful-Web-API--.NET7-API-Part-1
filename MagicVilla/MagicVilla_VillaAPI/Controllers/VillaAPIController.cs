145dcbb487b4c8d9802a1df9f8ea6988495b780c
Reggy Shicky <reginahshikanda@gmail.com>
2023-11-07 19:51:52 +0300

    DTOs, acting as an intermediary btn the entity and the data exposed via APIs

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,18 +1,20 @@
 ﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-    [Route("api/VillaAPI")]      //alternative [Route("api/[controller]")]   
+    //alternative [Route("api/[controller]")]
+    [Route("api/VillaAPI")]      //you can decide to use controller as a placeholder for the controller's name or use explicitly the name of the controller in this case it is VillaAPI 
     [ApiController]
     public class VillaAPIController  : ControllerBase  //base class provided by ASP.NET Core for building controllers in a Web API
     {
         [HttpGet] //notifies the swagger documentation that this endpoint is a GET endpoint
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa> {
-            new Villa{Id=1,Name="Pool View"},
-            new Villa{Id=2,Name="Beach View" }
+            return new List<VillaDTO> {
+            new VillaDTO{Id=1,Name="Pool View"},
+            new VillaDTO{Id=2,Name="Beach View" }
             };
         }
     }
