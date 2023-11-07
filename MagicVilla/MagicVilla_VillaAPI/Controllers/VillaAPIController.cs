328002bbc1135e884ea886123a2585de15b2a24c
Reggy Shicky <reginahshikanda@gmail.com>
2023-11-07 18:51:24 +0300

    First endpoint - HttpGet,

---
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,19 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController  : ControllerBase  //base class provided by ASP.NET Core for building controllers in a Web API
+    {
+        [HttpGet] //notifies the swagger documentation that this endpoint is a GET endpoint
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+            new Villa{Id=1,Name="Pool View"},
+            new Villa{Id=2,Name="Beach View" }
+            };
+        }
+    }
+}
